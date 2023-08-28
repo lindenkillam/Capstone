36520c728cb0af0547cfc274af81a39480c6adad
lindenkillam <77918412+lindenkillam@users.noreply.github.com>
2023-08-28 17:29:02 -0400

    Start on the Bahama Deck and some initial fiddling with the player movement
    Still a lot of work to do on the Caribbean Deck

--- CapstoneStart/Assets/PlayerController.cs
+++ CapstoneStart/Assets/PlayerController.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameManager gm;
+    /*
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDirection;
+    Rigidbody rb;
+    public Transform orientation;
+    */
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -17,6 +24,12 @@ public class PlayerController : MonoBehaviour
     {
         float hAxis = Input.GetAxis("Horizontal");
         float vAxis = Input.GetAxis("Vertical");
+
+        //horizontalInput = Input.GetAxisRaw("Horizontal");
+        //verticalInput = Input.GetAxisRaw("Vertical");
+
+        //moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        //rb.AddForce(moveDirection.normalized * gm.runSpeed/* * 10f*/, ForceMode.Force);
 
         gameObject.transform.Translate(gameObject.transform.forward * Time.deltaTime * gm.runSpeed * vAxis, Space.World);
         gameObject.transform.Rotate(0, gm.rotateSpeed * Time.deltaTime * hAxis, 0);
