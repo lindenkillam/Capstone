3c44d975abf59bfc39d21d1571816d2a3561d629
lindenkillam <77918412+lindenkillam@users.noreply.github.com>
2023-09-02 03:01:58 -0400

    The start of stairs

--- CapstoneStart/Assets/PlayerController.cs
+++ CapstoneStart/Assets/PlayerController.cs
@@ -5,33 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameManager gm;
-    /*
     float horizontalInput;
     float verticalInput;
     Vector3 moveDirection;
     Rigidbody rb;
     public Transform orientation;
-    */
 
     // Start is called before the first frame update
     void Start()
     {
-        //rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hAxis = Input.GetAxis("Horizontal");
-        float vAxis = Input.GetAxis("Vertical");
+        //float hAxis = Input.GetAxis("Horizontal");
+        //float vAxis = Input.GetAxis("Vertical");
 
-        //horizontalInput = Input.GetAxisRaw("Horizontal");
-        //verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
-        //moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        //rb.AddForce(moveDirection.normalized * gm.runSpeed/* * 10f*/, ForceMode.Force);
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * gm.runSpeed/* * 10f*/, ForceMode.Force);
 
-        gameObject.transform.Translate(gameObject.transform.forward * Time.deltaTime * gm.runSpeed * vAxis, Space.World);
-        gameObject.transform.Rotate(0, gm.rotateSpeed * Time.deltaTime * hAxis, 0);
+        //gameObject.transform.Translate(gameObject.transform.forward * Time.deltaTime * gm.runSpeed * vAxis, Space.World);
+        //gameObject.transform.Rotate(0, gm.rotateSpeed * Time.deltaTime * hAxis, 0);
     }
 }
