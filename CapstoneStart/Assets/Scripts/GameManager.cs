8be0060573353b87601cd7799f0c68b2631e669c
lindenkillam <77918412+lindenkillam@users.noreply.github.com>
2024-02-04 18:47:41 -0500

    Put in a much better Character Controller, hopefully

--- CapstoneStart/Assets/Scripts/GameManager.cs
+++ CapstoneStart/Assets/Scripts/GameManager.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public float runSpeed;
-    public float rotateSpeed;
-    public PlayerController player;
-    
     // Start is called before the first frame update
     void Start()
     {
