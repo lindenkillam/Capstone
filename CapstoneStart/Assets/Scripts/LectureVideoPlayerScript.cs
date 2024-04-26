6c598e42cd903b0853ce5e6f3ca76af87c7c68e7
lindenkillam <lindenkillam@gmail.com>
2024-04-26 06:05:36 -0400

    Add different TVs

---
+++ CapstoneStart/Assets/Scripts/LectureVideoPlayerScript.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.Video;
+
+public class LectureVideoPlayerScript : MonoBehaviour
+{
+  private VideoPlayer videoPlayer;
+  public VideoManagerScript vm;
+  int lastLecture = -1, newLecture;
+
+  // Start is called before the first frame update
+  void Awake()
+  {
+    videoPlayer = this.GetComponent<UnityEngine.Video.VideoPlayer>();
+
+    newLecture = Random.Range(0, vm.lectures.Length);
+    Debug.Log("New lecture chosen: Lecture #" + newLecture);
+    lastLecture = newLecture;
+
+    videoPlayer.clip = vm.lectures[newLecture];
+    videoPlayer.Play();
+  }
+
+  void Update()
+  {
+    if(!videoPlayer.isPlaying && videoPlayer.isPrepared)
+    {
+        newLecture = Random.Range(0, vm.lectures.Length);
+
+        if(newLecture == lastLecture)
+          return;
+        
+        /*
+        do
+        {
+            newLecture = Random.Range(0, vm.lectures.Length);
+        } while(newLecture == lastLecture);
+        */
+
+        Debug.Log("New lecture chosen: Lecture #" + newLecture);
+        lastLecture = newLecture;
+
+        videoPlayer.clip = vm.lectures[newLecture];
+        videoPlayer.Play();
+    }
+  }
+}
--- CapstoneStart/Assets/Scripts/WelcomeVideoPlayer.cs
+++ CapstoneStart/Assets/Scripts/WelcomeVideoPlayer.cs
@@ -3,13 +3,14 @@ using UnityEngine.Video;
 
 public class WelcomeVideoPlayer : MonoBehaviour
 {
-    private VideoPlayer videoPlayer;
+  private VideoPlayer videoPlayer;
+  //[SerializeField] Raycasting raycasting;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+    videoPlayer = this.GetComponent<UnityEngine.Video.VideoPlayer>();
+  }
 
   private void OnTriggerStay(Collider other)
   {
