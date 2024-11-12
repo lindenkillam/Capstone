26198c0c059204e7b8ffc55a2cc4dfb181e86510
lindenkillam <77918412+lindenkillam@users.noreply.github.com>
2024-11-11 21:40:03 -0600

    Add enum to main menu script

--- CapstoneStart/Assets/Scripts/Managers/MainMenuScript.cs
+++ CapstoneStart/Assets/Scripts/Managers/MainMenuScript.cs
@@ -8,13 +8,35 @@ public class MainMenuScript : MonoBehaviour
     public TMP_FontAsset[] menuFonts;
     public GameObject weComeBackText, newGame,
         loadGame, options, credits, menuImage;
-    int fontNum;
     public bool fontsChanging;
+
+    private enum FontType
+    {
+        MomsTypeWriter = 0,
+        AuthorizedSignature = 1,
+        BadHandwriting = 2,
+        Chalkboard = 3,
+        HandOfTT = 4,
+        handwrited = 5,
+        Handwritten = 6,
+        IAmTheCrayonMaster = 7,
+        LatinaBold = 8,
+        Lilly = 9,
+        LTColoredPencil = 10,
+        Lyon = 11,
+        MyUnprofessionalHandwriting = 12,
+        OldSharpie = 13,
+        Quikhand = 14,
+        SummerPisces = 15,
+        WritingStuff = 16
+    }
+
+    FontType fontType;
 
     // Start is called before the first frame update
     void Start()
     {
-        fontNum = 0;
+        fontType = FontType.MomsTypeWriter;
     }
 
     // Update is called once per frame
@@ -22,17 +44,57 @@ public class MainMenuScript : MonoBehaviour
     {
         if(fontsChanging)
         {
-            fontNum = Random.Range(0, menuFonts.Length);
+            int fontInt = Random.Range(0, menuFonts.Length);
+            fontType = (FontType)fontInt;
+
             weComeBackText.GetComponent<TextMeshProUGUI>().font =
-                menuFonts[fontNum];
+                menuFonts[fontInt];
             newGame.GetComponent<TextMeshProUGUI>().font =
-                menuFonts[fontNum];
+                menuFonts[fontInt];
             loadGame.GetComponent<TextMeshProUGUI>().font =
-                menuFonts[fontNum];
+                menuFonts[fontInt];
             options.GetComponent<TextMeshProUGUI>().font =
-                menuFonts[fontNum];
+                menuFonts[fontInt];
             credits.GetComponent<TextMeshProUGUI>().font =
-                menuFonts[fontNum];
+                menuFonts[fontInt];
+            
+            switch(fontType)
+            {
+                case FontType.AuthorizedSignature:
+                    break;
+                case FontType.BadHandwriting:
+                    break;
+                case FontType.Chalkboard:
+                    break;
+                case FontType.HandOfTT:
+                    break;
+                case FontType.handwrited:
+                    break;
+                case FontType.Handwritten:
+                    break;
+                case FontType.IAmTheCrayonMaster:
+                    break;
+                case FontType.LatinaBold:
+                    break;
+                case FontType.Lilly:
+                    break;
+                case FontType.LTColoredPencil:
+                    break;
+                case FontType.Lyon:
+                    break;
+                case FontType.MyUnprofessionalHandwriting:
+                    break;
+                case FontType.OldSharpie:
+                    break;
+                case FontType.Quikhand:
+                    break;
+                case FontType.SummerPisces:
+                    break;
+                case FontType.WritingStuff:
+                    break;
+                default:
+                    break;
+            }
         }  
     }
 }
